e79d83b5a4cbd13b3383f72cfd4dfc292f8b6343
chendi <15395068264@163.com>
2019-09-01 19:42:36 +0800

    commit for copy orders

--- src/main/java/com/nidecai/managerndc/controller/SupportController.java
+++ src/main/java/com/nidecai/managerndc/controller/SupportController.java
@@ -13,7 +13,7 @@ import com.nidecai.managerndc.common.entitycommon.ResultDTO;
 import com.nidecai.managerndc.service.SupportService;
 
 @RestController
-@RequestMapping(value = "/jmanager/v1/support")
+@RequestMapping(value = "/jmanager1/v1/support")
 public class SupportController {
 
 	@Autowired
@@ -38,12 +38,14 @@ public class SupportController {
     @ConvenientStore(value = "createData")
     public ResultDTO<Object> createData(@RequestParam(name = "sctime", required = false) Integer sCtime,
     		@RequestParam(name = "ectime", required = false) Integer eCtime,@RequestParam(name = "days", required = false) Integer days
-    		,@RequestParam(name = "ordernum", required = false) Integer orderNum) throws BusinessException {
+    		,@RequestParam(name = "ordernum", required = false) Integer orderNum,@RequestParam(name = "marketid" , required = false) Integer marketId,
+    		@RequestParam(name="riderpay",required = false) Double riderPay,@RequestParam(name="minorigin" ,required = false) Double minOrigin,
+    		@RequestParam(name="maxorigin" ,required = false) Double maxOrigin) throws BusinessException {
     	try {
     		if ("pro".equals(env.getActiveProfiles()[0])) {
     			return ResultUtil.getSuccess(1);
     		}
-    		supportService.copyOrder(sCtime, eCtime, days, orderNum);
+    		supportService.copyOrder(sCtime, eCtime, days, orderNum,marketId,riderPay,minOrigin,maxOrigin);
     		return ResultUtil.getSuccess(1);
 		    } catch (Exception e) {
 			throw new BusinessException(CommonMessageEnum.SERVERERR.getCode(), e.getMessage());
