bb1e8703fbe1c251b74596831c60b9a24ccc55bf
李冬冬 <754224613@qq.com>
2019-10-26 17:04:13 +0800

    commit

--- src/main/java/com/nidecai/managerndc/common/shedule/MarketShedule.java
+++ src/main/java/com/nidecai/managerndc/common/shedule/MarketShedule.java
@@ -18,57 +18,58 @@ import java.util.List;
 @Component
 public class MarketShedule {
 
-	@Autowired
-	private CloseUpShopService closeUpShopService;
+    @Autowired
+    private CloseUpShopService closeUpShopService;
 
-	@Autowired
-	private MakertStatTagMapper makertStatTagMapper;
+    @Autowired
+    private MakertStatTagMapper makertStatTagMapper;
 
-	@Autowired
-	private MarketStatService marketStatService;
-	
-	@Scheduled(cron = "0 */1 * * * ?")
-	public void closeShop() {
-		closeUpShopService.closeShop();
-	}
+    @Autowired
+    private MarketStatService marketStatService;
 
-	@Scheduled(cron = "0 10 12 * * ?")
-	public void cronTime() {
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void closeShop() {
+        closeUpShopService.closeShop();
+    }
 
-		marketStatService.recordMarketStatDay();
-		Date date = new Date();
-		SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
+    @Scheduled(cron = "0 10 12 * * ?")
+    public void cronTime() {
 
-		Calendar instanceDay= Calendar.getInstance();
-		instanceDay.setTime(date);
-		int i = instanceDay.get(Calendar.DATE);
-		instanceDay.set(Calendar.DATE, i-1);
+        marketStatService.recordMarketStatDay();
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
 
-		List<MakertStatTag> makertStatTags = makertStatTagMapper.selectAll();
-		MakertStatTag makertStatTag = makertStatTags.get(makertStatTags.size() - 1);
-		SimpleDateFormat simpleDateFormatMonth = new SimpleDateFormat("yyyy-MM");
-		//获取上一个月月份
-		Calendar instanceMonth = Calendar.getInstance();
+        Calendar instanceDay = Calendar.getInstance();
+        instanceDay.setTime(date);
+        int i = instanceDay.get(Calendar.DATE);
+        instanceDay.set(Calendar.DATE, i - 1);
 
-		instanceMonth.setTime(date);
-		instanceMonth.set(Calendar.MONTH, instanceMonth.get(Calendar.MONTH) - 1); // 设置为上一个月
-		if(!makertStatTag.getMonthDate().equals(simpleDateFormatMonth.format(instanceMonth.getTime()))){
-			marketStatService.recordMarketStatMonth();
-		}
+        List<MakertStatTag> makertStatTags = makertStatTagMapper.selectAll();
+        MakertStatTag makertStatTag = makertStatTags.get(makertStatTags.size() - 1);
+        SimpleDateFormat simpleDateFormatMonth = new SimpleDateFormat("yyyy-MM");
+        //获取上一个月月份
+        Calendar instanceMonth = Calendar.getInstance();
 
-		SimpleDateFormat simpleDateFormatYear = new SimpleDateFormat("yyyy");
-		Calendar instanceYear = Calendar.getInstance();
-		instanceYear.setTime(date);
-		instanceYear.add(Calendar.YEAR, -1);
-		if(!makertStatTag.getYearDate().equals(simpleDateFormatYear.format(instanceYear.getTime()))){
-			marketStatService.recordMarketStatYear();
-		}
+        instanceMonth.setTime(date);
+        // 设置为上一个月
+        instanceMonth.set(Calendar.MONTH, instanceMonth.get(Calendar.MONTH) - 1);
+        if (null != makertStatTag.getMonthDate() && !makertStatTag.getMonthDate().equals(simpleDateFormatMonth.format(instanceMonth.getTime()))) {
+            marketStatService.recordMarketStatMonth();
+        }
 
-		MakertStatTag makertStatbean = new MakertStatTag();
-		makertStatbean.setDayDate(simpleDateFormatDay.format(instanceDay.getTime()));
-		makertStatbean.setMonthDate(simpleDateFormatMonth.format(instanceMonth.getTime()));
-		makertStatbean.setYearDate(simpleDateFormatYear.format(instanceYear.getTime()));
-		makertStatTagMapper.insertSelective(makertStatbean);
+        SimpleDateFormat simpleDateFormatYear = new SimpleDateFormat("yyyy");
+        Calendar instanceYear = Calendar.getInstance();
+        instanceYear.setTime(date);
+        instanceYear.add(Calendar.YEAR, -1);
+        if (null != makertStatTag.getYearDate() && !makertStatTag.getYearDate().equals(simpleDateFormatYear.format(instanceYear.getTime()))) {
+            marketStatService.recordMarketStatYear();
+        }
 
-	}
+        MakertStatTag makertStatbean = new MakertStatTag();
+        makertStatbean.setDayDate(simpleDateFormatDay.format(instanceDay.getTime()));
+        makertStatbean.setMonthDate(simpleDateFormatMonth.format(instanceMonth.getTime()));
+        makertStatbean.setYearDate(simpleDateFormatYear.format(instanceYear.getTime()));
+        makertStatTagMapper.insertSelective(makertStatbean);
+
+    }
 }
--- src/main/java/com/nidecai/managerndc/service/impl/MarketStatServiceImpl.java
+++ src/main/java/com/nidecai/managerndc/service/impl/MarketStatServiceImpl.java
@@ -13,10 +13,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
+import java.util.*;
 
 /**
  * @Author: ldd
@@ -44,59 +41,76 @@ public class MarketStatServiceImpl implements MarketStatService {
     public void recordMarketStatDay() {
         try {
             //统计前一天订单
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            long current = System.currentTimeMillis();    //当前时间毫秒数
-            long endT = current / (1000 * 3600 * 24) * (1000 * 3600 * 24) - TimeZone.getDefault().getRawOffset();  //今天零点零分零秒的毫秒数
-            String end = simpleDateFormat.format(endT);
-            long zeroT = endT - 24 * 60 * 60 * 1000;
-            String start = simpleDateFormat.format(zeroT);
-            long timeStart = (simpleDateFormat.parse(start).getTime() / 1000);
-            long timeEnd = (simpleDateFormat.parse(end).getTime() / 1000);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date); // 设置为当前时间
+            calendar.add(Calendar.DATE, -1);//-1.昨天时间 0.当前时间 1.明天时间 *以此类推
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            date = calendar.getTime();
+            String accDate = format.format(date);    //上一个月时间为00：00：00开始
+            long timeStart = format.parse(accDate).getTime() / 1000;
+
+            Date last = new Date();
+            Calendar calendarLast = Calendar.getInstance();
+            calendarLast.setTime(last);
+            calendarLast.add(Calendar.DATE, -1);//-1.昨天时间 0.当前时间 1.明天时间 *以此类推
+            calendarLast.set(Calendar.HOUR_OF_DAY, 23);
+            calendarLast.set(Calendar.MINUTE, 59);
+            calendarLast.set(Calendar.SECOND, 59);
+            String end = format.format(calendarLast.getTime());
+            long timeEnd = format.parse(end).getTime() / 1000;
             String currentTime = DateUtil.getCurrentTimeByDay(new Date());
 
             List<MakertStatResultDTO> maketStatDay = riderOrderMapper.findMaketStatDay(timeStart, timeEnd);
             //查出所有状态为1的菜场.
             List<Integer> marketId = marketMapper.selectMarketId((byte) 1);
-            for (Integer integer : marketId) {
-                for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
-                    //判断昨天菜场是否下过单如果不等于则没有下过单.
-                    MakertStatDay marketStatDay = new MakertStatDay();
-                    if (!integer.equals(makertStatResultDTO.getMarketId())) {
-                        marketStatDay.setMarketid(integer);
-                        marketStatDay.setOrderNum(0);
-                        marketStatDay.setOrderPay(new BigDecimal(0));
-                        marketStatDay.setOrderRiderPay(new BigDecimal(0));
-                        marketStatDay.setOrderCoupon(new BigDecimal(0));
-                        marketStatDay.setOrderIntegral(new BigDecimal(0));
-                        marketStatDay.setOrderActivity(new BigDecimal(0));
-                        marketStatDay.setOrderVipRelief(new BigDecimal(0));
-                        marketStatDay.setDate(currentTime);
-                    } else {
-                        if (null == makertStatResultDTO.getOrderCoupon()) {
-                            makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderIntegral()) {
-                            makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderActivity()) {
-                            makertStatResultDTO.setOrderActivity(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderVipRelief()) {
-                            makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
-                        }
-                        marketStatDay.setMarketid(makertStatResultDTO.getMarketId());
-                        marketStatDay.setOrderNum(makertStatResultDTO.getOrderNum());
-                        marketStatDay.setOrderPay(makertStatResultDTO.getOrderPay());
-                        marketStatDay.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
-                        marketStatDay.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
-                        marketStatDay.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
-                        marketStatDay.setOrderActivity(makertStatResultDTO.getOrderActivity());
-                        marketStatDay.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
-                        marketStatDay.setDate(currentTime);
-                    }
-                    makertStatDayMapper.insertSelective(marketStatDay);
+            List list = new ArrayList();
+            for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
+                //判断昨天菜场是否下过单如果不等于则没有下过单.
+                MakertStatDay makertStatDay = new MakertStatDay();
+                if (null == makertStatResultDTO.getOrderCoupon()) {
+                    makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
                 }
+                if (null == makertStatResultDTO.getOrderIntegral()) {
+                    makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderActivity()) {
+                    makertStatResultDTO.setOrderActivity(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderVipRelief()) {
+                    makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
+                }
+                makertStatDay.setMarketid(makertStatResultDTO.getMarketId());
+                makertStatDay.setOrderNum(makertStatResultDTO.getOrderNum());
+                makertStatDay.setOrderPay(makertStatResultDTO.getOrderPay());
+                makertStatDay.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
+                makertStatDay.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
+                makertStatDay.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
+                makertStatDay.setOrderActivity(makertStatResultDTO.getOrderActivity());
+                makertStatDay.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
+                makertStatDay.setDate(currentTime);
+
+                list.add(makertStatResultDTO.getMarketId());
+                makertStatDayMapper.insertSelective(makertStatDay);
             }
+            marketId.removeAll(list);
+            for (Integer integer : marketId) {
+                MakertStatDay makertStatDay = new MakertStatDay();
+                makertStatDay.setMarketid(integer);
+                makertStatDay.setOrderNum(0);
+                makertStatDay.setOrderPay(new BigDecimal(0));
+                makertStatDay.setOrderRiderPay(new BigDecimal(0));
+                makertStatDay.setOrderCoupon(new BigDecimal(0));
+                makertStatDay.setOrderIntegral(new BigDecimal(0));
+                makertStatDay.setOrderActivity(new BigDecimal(0));
+                makertStatDay.setOrderVipRelief(new BigDecimal(0));
+                makertStatDay.setDate(currentTime);
+                makertStatDayMapper.insertSelective(makertStatDay);
+            }
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -143,46 +157,50 @@ public class MarketStatServiceImpl implements MarketStatService {
 
             //查出所有状态为1的菜场.
             List<Integer> marketId = marketMapper.selectMarketId((byte) 1);
-            for (Integer integer : marketId) {
-                for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
-                    //判断昨天菜场是否下过单如果不等于则没有下过单.
-                    MakertStatMonth makertStatMonth = new MakertStatMonth();
-                    if (!integer.equals(makertStatResultDTO.getMarketId())) {
-                        makertStatMonth.setMarketid(integer);
-                        makertStatMonth.setOrderNum(0);
-                        makertStatMonth.setOrderPay(new BigDecimal(0));
-                        makertStatMonth.setOrderRiderPay(new BigDecimal(0));
-                        makertStatMonth.setOrderCoupon(new BigDecimal(0));
-                        makertStatMonth.setOrderIntegral(new BigDecimal(0));
-                        makertStatMonth.setOrderActivity(new BigDecimal(0));
-                        makertStatMonth.setOrderVipRelief(new BigDecimal(0));
-                        makertStatMonth.setDate(currentTime);
-                    } else {
-                        if (null == makertStatResultDTO.getOrderCoupon()) {
-                            makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderIntegral()) {
-                            makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderActivity()) {
-                            makertStatResultDTO.setOrderActivity(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderVipRelief()) {
-                            makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
-                        }
-                        makertStatMonth.setMarketid(makertStatResultDTO.getMarketId());
-                        makertStatMonth.setOrderNum(makertStatResultDTO.getOrderNum());
-                        makertStatMonth.setOrderPay(makertStatResultDTO.getOrderPay());
-                        makertStatMonth.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
-                        makertStatMonth.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
-                        makertStatMonth.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
-                        makertStatMonth.setOrderActivity(makertStatResultDTO.getOrderActivity());
-                        makertStatMonth.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
-                        makertStatMonth.setDate(currentTime);
-                    }
-                    makertStatMonthMapper.insertSelective(makertStatMonth);
+            List list = new ArrayList();
+            for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
+                //判断昨天菜场是否下过单如果不等于则没有下过单.
+                MakertStatMonth makertStatMonth = new MakertStatMonth();
+                if (null == makertStatResultDTO.getOrderCoupon()) {
+                    makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
                 }
+                if (null == makertStatResultDTO.getOrderIntegral()) {
+                    makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderActivity()) {
+                    makertStatResultDTO.setOrderActivity(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderVipRelief()) {
+                    makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
+                }
+                makertStatMonth.setMarketid(makertStatResultDTO.getMarketId());
+                makertStatMonth.setOrderNum(makertStatResultDTO.getOrderNum());
+                makertStatMonth.setOrderPay(makertStatResultDTO.getOrderPay());
+                makertStatMonth.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
+                makertStatMonth.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
+                makertStatMonth.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
+                makertStatMonth.setOrderActivity(makertStatResultDTO.getOrderActivity());
+                makertStatMonth.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
+                makertStatMonth.setDate(currentTime);
+
+                list.add(makertStatResultDTO.getMarketId());
+                makertStatMonthMapper.insertSelective(makertStatMonth);
             }
+            marketId.removeAll(list);
+            for (Integer integer : marketId) {
+                MakertStatMonth makertStatMonth = new MakertStatMonth();
+                makertStatMonth.setMarketid(integer);
+                makertStatMonth.setOrderNum(0);
+                makertStatMonth.setOrderPay(new BigDecimal(0));
+                makertStatMonth.setOrderRiderPay(new BigDecimal(0));
+                makertStatMonth.setOrderCoupon(new BigDecimal(0));
+                makertStatMonth.setOrderIntegral(new BigDecimal(0));
+                makertStatMonth.setOrderActivity(new BigDecimal(0));
+                makertStatMonth.setOrderVipRelief(new BigDecimal(0));
+                makertStatMonth.setDate(currentTime);
+                makertStatMonthMapper.insertSelective(makertStatMonth);
+            }
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -229,45 +247,48 @@ public class MarketStatServiceImpl implements MarketStatService {
 
             //查出所有状态为1的菜场.
             List<Integer> marketId = marketMapper.selectMarketId((byte) 1);
-            for (Integer integer : marketId) {
-                for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
-                    //判断昨天菜场是否下过单如果不等于则没有下过单.
-                    MakertStatYear makertStatYear = new MakertStatYear();
-                    if (!integer.equals(makertStatResultDTO.getMarketId())) {
-                        makertStatYear.setMarketid(integer);
-                        makertStatYear.setOrderNum(0);
-                        makertStatYear.setOrderPay(new BigDecimal(0));
-                        makertStatYear.setOrderRiderPay(new BigDecimal(0));
-                        makertStatYear.setOrderCoupon(new BigDecimal(0));
-                        makertStatYear.setOrderIntegral(new BigDecimal(0));
-                        makertStatYear.setOrderActivity(new BigDecimal(0));
-                        makertStatYear.setOrderVipRelief(new BigDecimal(0));
-                        makertStatYear.setDate(currentTime);
-                    } else {
-                        if (null == makertStatResultDTO.getOrderCoupon()) {
-                            makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderIntegral()) {
-                            makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderActivity()) {
-                            makertStatResultDTO.setOrderActivity(new BigDecimal(0));
-                        }
-                        if (null == makertStatResultDTO.getOrderVipRelief()) {
-                            makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
-                        }
-                        makertStatYear.setMarketid(makertStatResultDTO.getMarketId());
-                        makertStatYear.setOrderNum(makertStatResultDTO.getOrderNum());
-                        makertStatYear.setOrderPay(makertStatResultDTO.getOrderPay());
-                        makertStatYear.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
-                        makertStatYear.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
-                        makertStatYear.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
-                        makertStatYear.setOrderActivity(makertStatResultDTO.getOrderActivity());
-                        makertStatYear.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
-                        makertStatYear.setDate(currentTime);
-                    }
-                    makertStatYearMapper.insertSelective(makertStatYear);
+            List list = new ArrayList();
+            for (MakertStatResultDTO makertStatResultDTO : maketStatDay) {
+                //判断昨天菜场是否下过单如果不等于则没有下过单.
+                MakertStatYear makertStatYear = new MakertStatYear();
+                if (null == makertStatResultDTO.getOrderCoupon()) {
+                    makertStatResultDTO.setOrderCoupon(new BigDecimal(0));
                 }
+                if (null == makertStatResultDTO.getOrderIntegral()) {
+                    makertStatResultDTO.setOrderIntegral(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderActivity()) {
+                    makertStatResultDTO.setOrderActivity(new BigDecimal(0));
+                }
+                if (null == makertStatResultDTO.getOrderVipRelief()) {
+                    makertStatResultDTO.setOrderVipRelief(new BigDecimal(0));
+                }
+                makertStatYear.setMarketid(makertStatResultDTO.getMarketId());
+                makertStatYear.setOrderNum(makertStatResultDTO.getOrderNum());
+                makertStatYear.setOrderPay(makertStatResultDTO.getOrderPay());
+                makertStatYear.setOrderRiderPay(makertStatResultDTO.getOrderRiderPay());
+                makertStatYear.setOrderCoupon(makertStatResultDTO.getOrderCoupon());
+                makertStatYear.setOrderIntegral(makertStatResultDTO.getOrderIntegral());
+                makertStatYear.setOrderActivity(makertStatResultDTO.getOrderActivity());
+                makertStatYear.setOrderVipRelief(makertStatResultDTO.getOrderVipRelief());
+                makertStatYear.setDate(currentTime);
+
+                list.add(makertStatResultDTO.getMarketId());
+                makertStatYearMapper.insertSelective(makertStatYear);
+            }
+            marketId.removeAll(list);
+            for (Integer integer : marketId) {
+                MakertStatYear makertStatYear = new MakertStatYear();
+                makertStatYear.setMarketid(integer);
+                makertStatYear.setOrderNum(0);
+                makertStatYear.setOrderPay(new BigDecimal(0));
+                makertStatYear.setOrderRiderPay(new BigDecimal(0));
+                makertStatYear.setOrderCoupon(new BigDecimal(0));
+                makertStatYear.setOrderIntegral(new BigDecimal(0));
+                makertStatYear.setOrderActivity(new BigDecimal(0));
+                makertStatYear.setOrderVipRelief(new BigDecimal(0));
+                makertStatYear.setDate(currentTime);
+                makertStatYearMapper.insertSelective(makertStatYear);
             }
         } catch (ParseException e) {
             e.printStackTrace();
